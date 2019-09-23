d1482fe601cbef48daab886864da4c8a7aa62991
naouezuki <nao6up@gmail.com>
2019-09-23 16:21:01 +0900

    GroupEditPage

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -20,43 +20,54 @@ $(document).on('turbolinks:load', function(){
       search_list.append(html);
   }
 
-  // function (){
-  //   var 
-  //     member_list.append(html);
-  // }
+  function appendUser(user_id, name){
+    var html = 
+      `<div class='chat-group-user'>
+        <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+        <p class='chat-group-user__name'>${name}</p>
+        <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+      </div>`
+      member_list.append(html);
+  }
 
-$('#user-search-field').on('keyup', function(){
-  var input = $("#user-search-field").val();
-  
 
-  $.ajax({
-    type: 'GET',
-    url:  '/users',
-    data: { keyword: input},
-    dataType: 'json'
-  })
+  $('#user-search-field').on('keyup', function(){
+    var input = $("#user-search-field").val();
+    
 
-  .done(function(users){
-    $('#user-search-result').empty();
-    if (users.length !== 0) {
-      users.forEach(function(user){
-        appendUserSearchList(user);
-      });
-    }
+    $.ajax({
+      type: 'GET',
+      url:  '/users',
+      data: { keyword: input},
+      dataType: 'json'
+    })
 
-    // else if (input.length !== 0){
-    //     users.forEach(function(user){
-    //         appendUser(user)
-    //     });
-    // }
+    .done(function(users){
+      $('#user-search-result').empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUserSearchList(user);
+        });
+      }
 
-    else {
-      appendErrMsgToHTML("一致するユーザーが見つかりません");
+      else {
+        appendErrMsgToHTML("一致するユーザーが見つかりません");
     }
   })
 
   .fail(function() {
     alert('ユーザー検索に失敗しました');
   })
-});
+  });
+
+  $('#user-search-result').on('click','.user-search-add',function(){
+    var user_id = $(this).data('user-id');
+    var name = $(this).data('user-name');
+    appendUser(user_id, name);
+    $(this).parent().remove();
+  })
+
+  $('#member-append').on('click','.user-search-remove',function(){
+    $('#member-append').empty();
+  })
 });
