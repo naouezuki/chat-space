d21f11ff2b9ea6686f4e5a75ba2f7c8ad99e74ef
naouezuki <nao6up@gmail.com>
2019-09-28 12:51:18 +0900

    addAcctionRequest

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,17 +22,17 @@ $(function(){
   };
 
 $('.js-form').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html);
@@ -44,5 +44,25 @@ $('.js-form').on('submit', function(e){
   });
   return false;
   });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message').last().data("id");
+    var group_id = $(".current-group").data("group-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: '/groups/' + group_id + '/api/messages'
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
 });
 
