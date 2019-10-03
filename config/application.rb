ba47b9247d503309a2db27ea309ec5694aa5a431
naouezuki <nao6up@gmail.com>
2019-10-03 20:27:45 +0900

    eraselines

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module ChatSpace
       g.helper false
       g.test_framework false
       config.i18n.default_locale = :ja
-      config.time_zone = ‘Tokyo’
+      config.time_zone = 'Tokyo'
       config.active_record.default_timezone = :local
       config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     end
